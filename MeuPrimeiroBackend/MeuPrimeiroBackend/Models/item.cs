67d9c0c7a1c7bb196b50ef10a065b4ce5f83dbb9
Thomas <Thomas@DESKTOP-AOGVH8E>
2017-10-18 21:14:56 -0200

    item

---
+++ MeuPrimeiroBackend/MeuPrimeiroBackend/Models/item.cs
@@ -0,0 +1,17 @@
+﻿namespace MeuPrimeiroBackend.Models
+{
+    public class item
+    {
+        public int ItemID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int DanoMaximo { get; set; }
+
+        public int DanoMinimo { get; set; }
+
+        public int Durabilidade { get; set; }
+    }
+}
